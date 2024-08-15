f12acb30de832f963bbdc8a8ff526396412059d8
a <saqooe@ya.ru>
2024-08-16 00:42:51 +0300

    Task1 solved

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson2
+
+fun main() {
+    val countOfOrders: Int = 75
+    val thanksText: String = "Благодарим за покупку наших беспроводных душевых леек!"
+
+    println(countOfOrders)
+    println(thanksText)
+}
