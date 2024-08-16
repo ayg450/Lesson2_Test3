57d8cd130ca194d71636706ca5cec9f7f9c4ebec
a <saqooe@ya.ru>
2024-08-16 22:54:27 +0300

    Task3 refactor

---
+++ src/main/kotlin/lesson2/lesson2_task3_refactor.kt
@@ -0,0 +1,13 @@
+package org.example.lesson2
+
+fun main() {
+    val year: Int = 1961
+    var hour = "09"
+    var minute = "07"
+    println("Год полета – " + year)
+    println("Взлет - " + hour + ":" + minute)
+
+    hour = "10"
+    minute = "55"
+    print("Посадка – " + hour + ":" + minute)
+}
